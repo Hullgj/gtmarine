5c1d4705fdfa517ff6510ccb27de8bb4878e64d6
Jasnhl <jason.douglas.hull@gmail.com>
2015-08-13 10:36:43 +0200

    Update
    Updated home and news

--- app/en/news.php
+++ app/en/news.php
@@ -9,18 +9,37 @@ echo $nav->getNav('bootstrap','GT Marine Offshore',false);
 	?>
 	<div class="landing">
 		<section class="container news">
-			<h1>News</h1>
+			<h1>News from the marine world.</h1>
 			<article>
-				<h2>News.Title</h2>
-				<p>
-					For those seeking jobs offshore, opportunities continue to to grow, even with the economic slow down. Half of current workforce in the oil and gas industry is retiring in the next 10 years. This is creating a huge labor shortage for the energy companies and drilling contractors, and huge opportunity for those seeking jobs offshore and on land in the oil and gas industry. There has never been a better time for either experienced or entry level workers to find jobs in this industry.
-				</p>
+				<div class="news-head">
+					<h2>News.Title</h2>
+					<h3 class="author">News.Author</h3>
+					<h4>News.Date</h4>
+					<div class="news-head-img">
+						<img src="app/src/img/Two-engineers-going-through.jpg" alt="Two Engineers going through final inspections">
+					</div>
+				</div>
+				<div class="news-body">
+					<p class="news-txt">
+						For those seeking jobs offshore, opportunities continue to to grow, even with the economic slow down. Half of current workforce in the oil and gas industry is retiring in the next 10 years. This is creating a huge labor shortage for the energy companies and drilling contractors, and huge opportunity for those seeking jobs offshore and on land in the oil and gas industry. There has never been a better time for either experienced or entry level workers to find jobs in this industry.
+					</p>
+					<a href="#" class="news-a">Find out more here!</a>
+				</div>
 			</article>
 			<article>
-				<h2>News.Title</h2>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur numquam aliquam, cupiditate possimus. Eum unde eveniet commodi dolorem eius et, minima pariatur similique quos recusandae cum fugiat illum ut laudantium a eos deleniti quis qui. Ipsam inventore, sit voluptatum explicabo ea. Natus quidem recusandae odio quaerat totam voluptatum, et neque a consequuntur quisquam qui magnam, hic earum, tempora repellendus quam. Quidem veniam ad maiores, delectus excepturi dicta reiciendis nobis pariatur laudantium, eum reprehenderit fugit quos, molestiae quasi labore. Hic quod fugiat mollitia maxime laborum sapiente soluta numquam ut repudiandae quaerat voluptatum nemo qui ipsum cum fuga ea, error maiores sequi iusto ducimus possimus quo ad. Quaerat molestias tempora ex nihil temporibus voluptate, numquam vitae! Beatae eligendi laudantium officia doloremque incidunt soluta animi voluptatum vel cupiditate. Sunt eaque quisquam sint dolor molestiae quasi neque sit optio impedit culpa iusto tenetur, quo rerum qui in error, nulla eum dolores maiores vero pariatur.
-				</p>
+				<div class="news-head">
+					<h2>News.Title</h2>
+					<h3 class="author">News.Author</h3>
+					<h4>News.Date</h4>
+					<div class="news-head-img">
+						<img src="app/src/img/Two-engineers-going-through.jpg" alt="Two Engineers going through final inspections">
+					</div>
+				</div>
+				<div class="news-body">
+					<p class="news-txt">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur numquam aliquam, cupiditate possimus. Eum unde eveniet commodi dolorem eius et, minima pariatur similique quos recusandae cum fugiat illum ut laudantium a eos deleniti quis qui. Ipsam inventore, sit voluptatum explicabo ea. Natus quidem recusandae odio quaerat totam voluptatum, et neque a consequuntur quisquam qui magnam, hic earum, tempora repellendus quam. Quidem veniam ad maiores, delectus excepturi dicta reiciendis nobis pariatur laudantium, eum reprehenderit fugit quos, molestiae quasi labore. Hic quod fugiat mollitia maxime laborum sapiente soluta numquam ut repudiandae quaerat voluptatum nemo qui ipsum cum fuga ea, error maiores sequi iusto ducimus possimus quo ad. Quaerat molestias tempora ex nihil temporibus voluptate, numquam vitae! Beatae eligendi laudantium officia doloremque incidunt soluta animi voluptatum vel cupiditate. Sunt eaque quisquam sint dolor molestiae quasi neque sit optio impedit culpa iusto tenetur, quo rerum qui in error, nulla eum dolores maiores vero pariatur.
+					</p>
+				</div>
 			</article>
 		</section>
 	</div>
